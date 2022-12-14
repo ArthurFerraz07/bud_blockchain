57354dc9e3e4b08b0c8b49c4274b94d5eacd81af
Arthur Ferraz <arthur.ferraz07@hotmail.com>
2022-12-14 18:33:32 -0300

    remove mongoid load from app.rb

--- app.rb
+++ app.rb
@@ -7,6 +7,4 @@ Dir['app/services/*.rb'].each { |file| require "./#{file}" }
 Dir['app/services/*/*.rb'].each { |file| require "./#{file}" }
 Dir['app/controllers/*/*/*.rb'].each { |file| require "./#{file}" }
 
-Mongoid.load!(File.join(File.dirname(__FILE__), 'config', 'mongoid.yml'))
-
 DefineConstantByHashConst.new(Api::V1::ApplicationController, 'RESPONSE_STATUSES').call
